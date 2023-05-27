5cd8022c6ca9c18b76aa54a7bd6e660718d3c390
sarahbroton <sarah.broton@gmail.com>
2023-05-26 20:23:55 -0500

    woohooooo-update06

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,5 @@
+console.log(dayjs()); 
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -21,3 +23,5 @@ $(function () {
     //
     // TODO: Add code to display the current date in the header of the page.
   });
+
+//   dayjs().format(dddd-MMMM-D); 
