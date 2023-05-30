88f55ceb333bcaed77bb8d14e18a8aee5f82999e
sarahbroton <sarah.broton@gmail.com>
2023-05-30 17:51:53 -0500

    updating

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,114 +5,83 @@
 
 $(document).ready(function () {
 
-// date display
-$('#currentDay').text(dayjs().format('dddd, MMMM D')); 
+    // date display
+    $('#currentDay').text(dayjs().format('dddd, MMMM D'));
 
-// PAST = GREY
-// PRESENT = RED
-// FUTURE = GREEN
+    // PAST = GREY
+    // PRESENT = RED
+    // FUTURE = GREEN
 
-// / CODE COMES FROM https://github.com/jkaho/workday-scheduler/commit/5385c23cb774f24100d32eec123204d3d4e41bd4#
-// PAST PRESENT FUTURE TIME COLOR BLOCKS
-var currentHour = dayjs().format('H');
-$(".time-block").each(function() {
-    if (parseInt(currentHour) === parseInt(this.id)) {
-        $(this).addClass("present");
-    } else if (parseInt(currentHour) > parseInt(this.id)) {
-        $(this).addClass("past");
-    } else {
-        $(this).addClass("future");
+    // / CODE COMES FROM https://github.com/jkaho/workday-scheduler/commit/5385c23cb774f24100d32eec123204d3d4e41bd4#
+    // PAST PRESENT FUTURE TIME COLOR BLOCKS
+    var currentHour = dayjs().format('H');
+    $(".time-block").each(function () {
+        if (parseInt(currentHour) === parseInt(this.id)) {
+            $(this).addClass("present");
+        } else if (parseInt(currentHour) > parseInt(this.id)) {
+            $(this).addClass("past");
+        } else {
+            $(this).addClass("future");
+        }
+    })
+
+    // CODE IS A MIX OF MY CODE AND  FROM https://github.com/jkaho/workday-scheduler/commit/5385c23cb774f24100d32eec123204d3d4e41bd4#
+    // PAST PRESENT FUTURE TIME BLOCKS
+
+    $(".saveBtn").each(function () {
+        if (parseInt(currentHour) === parseInt(this.id.split("-")[1])) {
+            $(this).addClass("present");
+        } else if (parseInt(currentHour) > parseInt(this.id.split("-")[1])) {
+            $(this).addClass("past");
+        } else {
+            $(this).addClass("future");
+        }
+    })
+
+
+    // CODE COMES FROM https://github.com/jkaho/workday-scheduler/commit/5385c23cb774f24100d32eec123204d3d4e41bd4#
+    // events
+    var storedEvents = [];
+
+    initialise();
+    function renderEvents() {
+        $("textarea").each(function () {
+            this.value = "";
+        })
+
+        $.each(storedEvents, function () {
+            $("textarea" + this.eventTime)[0].value = this.eventText;
+        })
     }
-})
+    function initialise() {
+        var userEvent = JSON.parse(localStorage.getItem("storedEvents"));
+        if (userEvent !== null) {
+            storedEvents = userEvent;
+        }
+        renderEvents();
+    }
 
-// CODE IS A MIX OF MY CODE AND  FROM https://github.com/jkaho/workday-scheduler/commit/5385c23cb774f24100d32eec123204d3d4e41bd4#
-// PAST PRESENT FUTURE TIME BLOCKS
+    // my code with help from tutor
+    $(".saveBtn").on("click", function () {
+        var eventName = $(this).siblings(".description").val();
+        var eventTime = $(this).parent().attr('id');
 
-$(".saveBtn").each(function() {
-  if (parseInt(currentHour) === parseInt(this.id.split("-")[1])) {
-      $(this).addClass("present");
-  } else if (parseInt(currentHour) > parseInt(this.id.split("-")[1])) {
-      $(this).addClass("past");
-  } else {
-      $(this).addClass("future");
-  }
-})
+        console.log(eventName, eventTime);
 
+        localStorage.setItem(eventTime, eventName);
+    })
 
-// CODE COMES FROM https://github.com/jkaho/workday-scheduler/commit/5385c23cb774f24100d32eec123204d3d4e41bd4#
-// events
-var storedEvents = [];
-
-initialise();
-function renderEvents() {
-  $("textarea").each(function() {
-      this.value = "";
-  })
-  
-  $.each(storedEvents, function() {
-      $("textarea" + this.eventTime)[0].value = this.eventText;
-  }) 
-}
-function initialise() {
-  var userEvent = JSON.parse(localStorage.getItem("storedEvents"));
-  if (userEvent !== null) {
-      storedEvents = userEvent;
-  }
-  renderEvents();
-}
-// function storeEvents() {
-//   localStorage.setItem("storedEvents", JSON.stringify(storedEvents));
-// }
-$(".saveBtn").on("click", function() {
-//   event.preventDefault();
-//   event.stopPropagation();
-  var eventName = $(this).siblings(".description").val(); 
-var eventTime = $(this).parent().attr('id');
-
-console.log(eventName, eventTime); 
-
-localStorage.setItem(eventTime, eventName); 
-})
-
-function renderEvents() {
-$("#hour-9 .description").val(localStorage.getItem("hour-9")); 
-$("#hour-10 .description").val(localStorage.getItem("hour-10"));
-$("#hour-11 .description").val(localStorage.getItem("hour-11"));
-}
-
-
-
-
-//   saving to local storage
-  
-//   var eventObject = {
-//       eventTime: className,
-//       eventText: $("textarea").val()
-//   } 
-//   console.log(eventObject.eventText);
-//   if (storedEvents.length > 0) {
-//       $.each(storedEvents, function() {
-//           if (this.eventTime === event.target.className) {
-//               storedEvents.splice($.inArray(this, storedEvents), 1);
-//           }    
-//       });
-//   }
-//   storedEvents.push(eventObject);
-//   $.each(storedEvents, function() {
-//       if (this.eventText === "") {
-//           storedEvents.splice($.inArray(this, storedEvents), 1);
-//       }
-//   })
-//   storeEvents();
-//   renderEvents();
-
-
-
-
-
-
-
-
+    function renderEvents() {
+        $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+        $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+        $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+        $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+        $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+        $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+        $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+        $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+        $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+    }
 })
 
 
